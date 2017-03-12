5f8df7ecd7504cbc16c09a7c44f3572a684b2966
Matthew Hitchcock <matthitchcock@outlook.com>
2017-03-12 17:52:51 +0800

    First commit
    Initial functions for module

---
+++ xNetworkAdapterAlias.psm1
@@ -0,0 +1,65 @@
+﻿Function Get-xNetworkAdapterAlias
+{
+    [CmdletBinding()]
+    Param(
+        [String]$MacAddress,
+        [String]$AliasName
+    )
+
+    [Array]$Adapter = (NetAdapter\Get-NetAdapter) | Where-Object {$PSItem.MacAddress -eq $MacAddress}
+
+    if ($Adapter.Count -eq 1)
+    {
+
+        $Output = @{
+            MacAddress = $MacAddress
+            AliasName = $Adapter.Name
+        }
+
+        Return $Output
+    }
+    else
+    {
+        Write-Error -Message "Could not identify correct NetAdapter"
+    }
+}
+
+Function Set-xNetworkAdapterAlias
+{
+    [CmdletBinding()]
+    Param(
+        [String]$MacAddress,   
+        [String]$AliasName
+    )
+
+    [Array]$Adapter = (NetAdapter\Get-NetAdapter) | Where-Object {$PSItem.MacAddress -eq $MacAddress}
+
+    if ($Adapter.Count -eq 1)
+    {
+        NetAdapter\Rename-NetAdapter -Name $Adapter[0].Name -NewName $AliasName
+    }
+    else
+    {
+        Write-Error -Message "Could not identify correct NetAdapter"
+    }
+}
+
+Function Test-xNetworkAdapterAlias
+{
+    [CmdletBinding()]
+    Param(
+        [String]$MacAddress,
+        [String]$AliasName
+    )
+
+    [Boolean]$Result = $false
+
+    if (((NetAdapter\Get-NetAdapter) | Where-Object {$psitem.MacAddress -eq $MacAddress}).Name -eq $AliasName)
+    {
+        $Result = $true
+    }
+
+    Return $Result
+}
+
+Export-ModuleMember *
